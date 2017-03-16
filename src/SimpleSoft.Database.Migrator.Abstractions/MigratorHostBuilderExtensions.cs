e426324d0cea2715294d0e3f235da0747b7ede3d
João Simões <nogravity00@outlook.com>
2017-03-16 00:28:38 +0000

    Added Configure, UseServiceProvider and UseLoggerFactory extensions to IMigratorHostBuilder

---
+++ src/SimpleSoft.Database.Migrator.Abstractions/MigratorHostBuilderExtensions.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleSoft.Database.Migrator
+{
+    /// <summary>
+    /// Extension methods for <see cref="IMigratorHostBuilder"/>
+    /// </summary>
+    public static class MigratorHostBuilderExtensions
+    {
+        #region Configure
+
+        /// <summary>
+        /// Adds the handler to the <see cref="IMigratorHostBuilder.ConfigurationHandlers"/> collection.
+        /// </summary>
+        /// <typeparam name="TBuilder">The builder type</typeparam>
+        /// <param name="builder">The builder instance</param>
+        /// <param name="handler">The handler to add</param>
+        /// <returns>The builder instance</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static TBuilder Configure<TBuilder>(this TBuilder builder, Action<IServiceProvider, ILoggerFactory> handler)
+            where TBuilder : IMigratorHostBuilder
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.AddConfigurator(handler);
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds the handler to the <see cref="IMigratorHostBuilder.ConfigurationHandlers"/> collection.
+        /// </summary>
+        /// <typeparam name="TBuilder">The builder type</typeparam>
+        /// <param name="builder">The builder instance</param>
+        /// <param name="handler">The handler to add</param>
+        /// <returns>The builder instance</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static TBuilder Configure<TBuilder>(this TBuilder builder, Action<IServiceProvider> handler)
+            where TBuilder : IMigratorHostBuilder
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (handler == null) throw new ArgumentNullException(nameof(handler));
+
+            builder.AddConfigurator((provider, factory) =>
+            {
+                handler(provider);
+            });
+            return builder;
+        }
+
+        #endregion
+
+        #region UseServiceProvider
+
+        /// <summary>
+        /// Uses the given handler to build the <see cref="IServiceProvider"/> that
+        /// will be used by the <see cref="IMigratorHost"/> to build.
+        /// </summary>
+        /// <typeparam name="TBuilder">The builder type</typeparam>
+        /// <param name="builder">The builder instance</param>
+        /// <param name="buildServiceProvider">The builder function</param>
+        /// <returns>The builder instance</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static TBuilder UseServiceProvider<TBuilder>(this TBuilder builder, Func<IServiceCollection, ILoggerFactory, IServiceProvider> buildServiceProvider)
+            where TBuilder : IMigratorHostBuilder
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.SetServiceProviderBuilder(buildServiceProvider);
+            return builder;
+        }
+
+        /// <summary>
+        /// Uses the given handler to build the <see cref="IServiceProvider"/> that
+        /// will be used by the <see cref="IMigratorHost"/> to build.
+        /// </summary>
+        /// <typeparam name="TBuilder">The builder type</typeparam>
+        /// <param name="builder">The builder instance</param>
+        /// <param name="buildServiceProvider">The builder function</param>
+        /// <returns>The builder instance</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static TBuilder UseServiceProvider<TBuilder>(this TBuilder builder, Func<IServiceCollection, IServiceProvider> buildServiceProvider)
+            where TBuilder : IMigratorHostBuilder
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (buildServiceProvider == null) throw new ArgumentNullException(nameof(buildServiceProvider));
+
+            builder.SetServiceProviderBuilder((provider, factory) => buildServiceProvider(provider));
+            return builder;
+        }
+
+        /// <summary>
+        /// Uses the given handler to build the <see cref="IServiceProvider"/> that
+        /// will be used by the <see cref="IMigratorHost"/> to build.
+        /// </summary>
+        /// <typeparam name="TBuilder">The builder type</typeparam>
+        /// <param name="builder">The builder instance</param>
+        /// <param name="buildServiceProvider">The builder function</param>
+        /// <returns>The builder instance</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static TBuilder UseServiceProvider<TBuilder>(this TBuilder builder, Func<IServiceProvider> buildServiceProvider)
+            where TBuilder : IMigratorHostBuilder
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (buildServiceProvider == null) throw new ArgumentNullException(nameof(buildServiceProvider));
+
+            builder.SetServiceProviderBuilder((provider, factory) => buildServiceProvider());
+            return builder;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Assigns the given <see cref="ILoggerFactory"/> to be used
+        /// by the <see cref="IMigratorHost"/>.
+        /// </summary>
+        /// <typeparam name="TBuilder">The builder type</typeparam>
+        /// <param name="builder">The builder instance</param>
+        /// <param name="loggerFactory">The logger factory to use</param>
+        /// <returns>The builder instance</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static TBuilder UseLoggerFactory<TBuilder>(this TBuilder builder, ILoggerFactory loggerFactory)
+            where TBuilder : IMigratorHostBuilder
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.SetLoggerFactory(loggerFactory);
+            return builder;
+        }
+    }
+}
