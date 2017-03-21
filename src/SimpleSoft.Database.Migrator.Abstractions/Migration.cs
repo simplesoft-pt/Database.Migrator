90558ae8c2ec8ee44b6d6f788d1a211f291663c7
João Simões <nogravity00@outlook.com>
2017-03-21 18:04:51 +0000

    Abstract implementation for migration

---
+++ src/SimpleSoft.Database.Migrator.Abstractions/Migration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Database.Migrator
+{
+    /// <summary>
+    /// Represents a migration
+    /// </summary>
+    /// <typeparam name="TContext">The migration context</typeparam>
+    public abstract class Migration<TContext> : IMigration<TContext> 
+        where TContext : IMigrationContext
+    {
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="context">The migration context</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        protected Migration(TContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            Context = context;
+        }
+
+        /// <inheritdoc />
+        public TContext Context { get; }
+
+        /// <inheritdoc />
+        public virtual Task ApplyAsync(CancellationToken ct)
+        {
+            return Task.FromResult(true);
+        }
+
+        /// <inheritdoc />
+        public virtual Task RollbackAsync(CancellationToken ct)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
