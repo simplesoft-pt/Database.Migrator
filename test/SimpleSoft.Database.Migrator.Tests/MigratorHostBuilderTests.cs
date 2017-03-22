878451693a081c641d0ec70143bdf1ae571707b2
João Simões <nogravity00@outlook.com>
2017-03-22 00:17:11 +0000

    Added tests for logging factory configurations

---
+++ test/SimpleSoft.Database.Migrator.Tests/MigratorHostBuilderTests.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace SimpleSoft.Database.Migrator.Tests
+{
+    public class MigratorHostBuilderTests
+    {
+        #region LoggerFactory
+
+        [Fact]
+        public void GivenAHostBuilderWithDefaultValuesThenLoggerFactoryMustNotBeNull()
+        {
+            ILoggerFactory factory = null;
+
+            using (var builder = new MigratorHostBuilder())
+            {
+                builder.AddLoggingConfigurator(f =>
+                {
+                    factory = f;
+                });
+                builder.Build();
+
+                Assert.NotNull(factory);
+            }
+        }
+
+        [Fact]
+        public void GivenAHostBuilderWhenSettingACustomFactoryThenItMustBeUsed()
+        {
+            ILoggerFactory builderFactory = null;
+            var originalFactory = new LoggerFactory();
+
+            using (var builder = new MigratorHostBuilder())
+            {
+                builder.SetLoggerFactory(originalFactory);
+                builder.AddLoggingConfigurator(f =>
+                {
+                    builderFactory = f;
+                });
+                builder.Build();
+
+                Assert.NotNull(builderFactory);
+                Assert.Same(originalFactory, builderFactory);
+            }
+        }
+
+        [Fact]
+        public void GivenAHostBuilderWhenUsingCustomFactoryThenItMustBeUsed()
+        {
+            ILoggerFactory builderFactory = null;
+            var originalFactory = new LoggerFactory();
+
+            using (var builder = new MigratorHostBuilder()
+                .UseLoggerFactory(originalFactory))
+            {
+                builder.AddLoggingConfigurator(f =>
+                {
+                    builderFactory = f;
+                });
+                builder.Build();
+
+                Assert.NotNull(builderFactory);
+                Assert.Same(originalFactory, builderFactory);
+            }
+        }
+
+        [Fact]
+        public void GivenAHostBuilderWhenAddingLoggingConfiguratorsThenAllHandlersMustBeRun()
+        {
+            var runCount = 0;
+
+            using (var builder = new MigratorHostBuilder())
+            {
+                builder.AddLoggingConfigurator(f =>
+                {
+                    ++runCount;
+                });
+                builder.AddLoggingConfigurator(f =>
+                {
+                    ++runCount;
+                });
+                builder.Build();
+
+                Assert.NotEmpty(builder.LoggingConfigurationHandlers);
+                Assert.Equal(2, builder.LoggingConfigurationHandlers.Count);
+                Assert.Equal(2, runCount);
+            }
+        }
+
+        [Fact]
+        public void GivenAHostBuilderWhenConfigureLoggingThenAllHandlersMustBeRun()
+        {
+            var runCount = 0;
+
+            using (var builder = new MigratorHostBuilder()
+                .ConfigureLogging(f =>
+                {
+                    ++runCount;
+                })
+                .ConfigureLogging(f =>
+                {
+                    ++runCount;
+                }))
+            {
+                builder.Build();
+
+                Assert.NotEmpty(builder.LoggingConfigurationHandlers);
+                Assert.Equal(2, builder.LoggingConfigurationHandlers.Count);
+                Assert.Equal(2, runCount);
+            }
+        }
+
+        #endregion
+    }
+}
