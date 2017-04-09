84a1b840d0da0bc0b5da3cc5b03c7eb0693478e3
João Simões <nogravity00@outlook.com>
2017-04-09 19:18:34 +0100

    Added extensions to register migrations

--- src/SimpleSoft.Database.Migrator.Abstractions/ServiceCollectionExtensions.cs
+++ src/SimpleSoft.Database.Migrator.Abstractions/ServiceCollectionExtensions.cs
@@ -23,6 +23,7 @@ namespace SimpleSoft.Database.Migrator
             if (services == null) throw new ArgumentNullException(nameof(services));
 
             services.AddScoped<TContext>();
+            services.AddScoped<IMigrationContext, TContext>();
             return services;
         }
 
@@ -42,6 +43,7 @@ namespace SimpleSoft.Database.Migrator
             if (context == null) throw new ArgumentNullException(nameof(context));
 
             services.AddScoped(k => context);
+            services.AddScoped<IMigrationContext, TContext>(k => context);
             return services;
         }
 
@@ -61,7 +63,121 @@ namespace SimpleSoft.Database.Migrator
             if (builder == null) throw new ArgumentNullException(nameof(builder));
 
             services.AddScoped(builder);
+            services.AddScoped<IMigrationContext, TContext>(builder);
             return services;
+        }
+
+        #endregion
+
+        #region AddMigration
+
+        /// <summary>
+        /// Registers an <see cref="IMigration{TContext}"/> type to the services collection. 
+        /// </summary>
+        /// <typeparam name="TMigration">The migration type</typeparam>
+        /// <typeparam name="TContext">The context type</typeparam>
+        /// <param name="services">The service collection</param>
+        /// <returns>The service collection after registration</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IServiceCollection AddMigration<TMigration, TContext>(this IServiceCollection services)
+            where TMigration : class, IMigration<TContext> 
+            where TContext : IMigrationContext
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddScoped<TMigration>();
+            services.AddScoped<IMigration<TContext>, TMigration>();
+            return services;
+        }
+
+        /// <summary>
+        /// Registers an <see cref="IMigration"/> type to the services collection. 
+        /// </summary>
+        /// <typeparam name="TMigration">The migration type</typeparam>
+        /// <param name="services">The service collection</param>
+        /// <returns>The service collection after registration</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IServiceCollection AddMigration<TMigration>(this IServiceCollection services)
+            where TMigration : class, IMigration
+        {
+            return services.AddMigration<TMigration, IMigrationContext>()
+                .AddScoped<IMigration, TMigration>();
+        }
+
+        /// <summary>
+        /// Registers an <see cref="IMigration{TContext}"/> type to the services collection. 
+        /// </summary>
+        /// <typeparam name="TMigration">The migration type</typeparam>
+        /// <typeparam name="TContext">The context type</typeparam>
+        /// <param name="services">The service collection</param>
+        /// <param name="migration">The migration instance</param>
+        /// <returns>The service collection after registration</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IServiceCollection AddMigration<TMigration, TContext>(
+            this IServiceCollection services, TMigration migration)
+            where TMigration : class, IMigration<TContext> 
+            where TContext : IMigrationContext
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (migration == null) throw new ArgumentNullException(nameof(migration));
+
+            services.AddScoped(k => migration);
+            services.AddScoped<IMigration<TContext>>(k => migration);
+            return services;
+        }
+
+        /// <summary>
+        /// Registers an <see cref="IMigration"/> type to the services collection. 
+        /// </summary>
+        /// <typeparam name="TMigration">The migration type</typeparam>
+        /// <param name="services">The service collection</param>
+        /// <param name="migration">The migration instance</param>
+        /// <returns>The service collection after registration</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IServiceCollection AddMigration<TMigration>(
+            this IServiceCollection services, TMigration migration)
+            where TMigration : class, IMigration
+        {
+            return services.AddMigration<TMigration, IMigrationContext>(migration)
+                .AddScoped<IMigration>(k => migration);
+        }
+
+        /// <summary>
+        /// Registers an <see cref="IMigration{TContext}"/> type to the services collection. 
+        /// </summary>
+        /// <typeparam name="TMigration">The migration type</typeparam>
+        /// <typeparam name="TContext">The context type</typeparam>
+        /// <param name="services">The service collection</param>
+        /// <param name="builder">The migration builder</param>
+        /// <returns>The service collection after registration</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IServiceCollection AddMigration<TMigration, TContext>(
+            this IServiceCollection services, Func<IServiceProvider,TMigration> builder)
+            where TMigration : class, IMigration<TContext> 
+            where TContext : IMigrationContext
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            services.AddScoped(builder);
+            services.AddScoped<IMigration<TContext>>(builder);
+            return services;
+        }
+
+        /// <summary>
+        /// Registers an <see cref="IMigration{TContext}"/> type to the services collection. 
+        /// </summary>
+        /// <typeparam name="TMigration">The migration type</typeparam>
+        /// <param name="services">The service collection</param>
+        /// <param name="builder">The migration builder</param>
+        /// <returns>The service collection after registration</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IServiceCollection AddMigration<TMigration>(
+            this IServiceCollection services, Func<IServiceProvider, TMigration> builder)
+            where TMigration : class, IMigration
+        {
+            return services.AddMigration<TMigration, IMigrationContext>(builder)
+                .AddScoped<IMigration>(builder);
         }
 
         #endregion
