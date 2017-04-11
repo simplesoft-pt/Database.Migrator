be2b7e6cb5b04e790d27d0d3e1d923b88d2f58c1
João Simões <nogravity00@outlook.com>
2017-04-11 11:53:56 +0100

    Added initial implementation for the relational context

---
+++ src/SimpleSoft.Database.Migrator.Relational/IRelationalMigrationContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace SimpleSoft.Database.Migrator.Relational
+{
+    /// <summary>
+    /// The relational migration context
+    /// </summary>
+    /// <typeparam name="TOptions">The migration options</typeparam>
+    public interface IRelationalMigrationContext<out TOptions> : IMigrationContext<TOptions>, IRelationalMigrationContext 
+        where TOptions : MigrationOptions
+    {
+
+    }
+
+    /// <summary>
+    /// The relational migration context
+    /// </summary>
+    public interface IRelationalMigrationContext : IMigrationContext
+    {
+        /// <summary>
+        /// The database connection
+        /// </summary>
+        IDbConnection Connection { get; }
+    }
+}
---
+++ src/SimpleSoft.Database.Migrator.Relational/RelationalMigrationContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+
+namespace SimpleSoft.Database.Migrator.Relational
+{
+    /// <summary>
+    /// The relational migration context
+    /// </summary>
+    public abstract class RelationalMigrationContext : MigrationContext, IRelationalMigrationContext
+    {
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="connection">The connection to use</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        protected RelationalMigrationContext(IDbConnection connection)
+        {
+            if (connection == null) throw new ArgumentNullException(nameof(connection));
+            Connection = connection;
+        }
+
+        #region Implementation of IRelationalMigrationContext
+
+        /// <inheritdoc />
+        public IDbConnection Connection { get; }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// The relational migration context
+    /// </summary>
+    /// <typeparam name="TOptions">The migration options</typeparam>
+    public class RelationalMigrationContext<TOptions> : MigrationContext<TOptions>, IRelationalMigrationContext<TOptions>
+        where TOptions : MigrationOptions
+    {
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="connection">The connection to use</param>
+        /// <param name="options">The context options</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public RelationalMigrationContext(IDbConnection connection, TOptions options) : base(options)
+        {
+            if (connection == null) throw new ArgumentNullException(nameof(connection));
+
+            Connection = connection;
+        }
+
+        #region Implementation of IRelationalMigrationContext
+
+        /// <inheritdoc />
+        public IDbConnection Connection { get; }
+
+        #endregion
+    }
+}
