3ee9e07b4753c30515aa051b977e016d4ed8d0cc
João Simões <nogravity00@outlook.com>
2017-04-11 16:31:09 +0100

    Inicial implementation of the relational manager

---
+++ src/SimpleSoft.Database.Migrator.Relational/RelationalMigrationManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleSoft.Database.Migrator.Relational
+{
+    /// <summary>
+    /// Manages migration states
+    /// </summary>
+    /// <typeparam name="TContext">The context type</typeparam>
+    public class RelationalMigrationManager<TContext> : IMigrationManager<TContext> 
+        where TContext : IRelationalMigrationContext
+    {
+        private readonly ILogger<RelationalMigrationManager<TContext>> _logger;
+        private readonly string _contextTypeName;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="context">The migration context</param>
+        /// <param name="logger">The logger</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        protected RelationalMigrationManager(TContext context, 
+            ILogger<RelationalMigrationManager<TContext>> logger)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (logger == null) throw new ArgumentNullException(nameof(logger));
+
+            _logger = logger;
+            _contextTypeName = typeof(TContext).Name;
+
+            Context = context;
+        }
+
+        #region Implementation of IMigrationManager<out TOptions>
+
+        /// <inheritdoc />
+        public TContext Context { get; }
+
+        /// <inheritdoc />
+        public virtual async Task PrepareDatabaseAsync(CancellationToken ct)
+        {
+            _logger.LogDebug(
+                "Preparing context '{contextName}' database for migrations", _contextTypeName);
+
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<bool> AddMigrationAsync(string migrationId, string className, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<string> GetMostRecentMigrationIdAsync(CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<bool> RemoveMigrationAsync(string migrationId, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
