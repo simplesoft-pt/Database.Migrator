cc5d0b882df93e8a913e51a173e6f2ee85b874cb
João Simões <nogravity00@outlook.com>
2017-04-17 12:10:51 +0100

    Added assert for unexistant table

--- test/SimpleSoft.Database.Migrator.Tests/SqlServer/SqlServerMigrationManagerTests.cs
+++ test/SimpleSoft.Database.Migrator.Tests/SqlServer/SqlServerMigrationManagerTests.cs
@@ -13,9 +13,17 @@ namespace SimpleSoft.Database.Migrator.Tests.SqlServer
                 var manager = new SqlServerMigrationManager<MigratorTestContext>(
                     ctx, LoggingManager.CreateTestLogger<SqlServerMigrationManager<MigratorTestContext>>());
 
-                await manager.PrepareDatabaseAsync(ct);
-
                 var tableId = await manager.Context.RunAsync(async () =>
+                    await manager.Context.QuerySingleAsync<long?>(
+                        "SELECT OBJECT_ID(@TableName, 'U') as TableId", new
+                        {
+                            TableName = manager.MigrationsHistoryTableName
+                        }), ct);
+                Assert.Null(tableId);
+
+                await manager.PrepareDatabaseAsync(ct);
+                
+                tableId = await manager.Context.RunAsync(async () =>
                     await manager.Context.QuerySingleAsync<long?>(
                         "SELECT OBJECT_ID(@TableName, 'U') as TableId", new
                         {
