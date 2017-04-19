a4863cdeefdfab77a59013435df1e98530055168
João Simões <nogravity00@outlook.com>
2017-04-19 17:36:31 +0100

    Added test for argument null exception

---
+++ test/SimpleSoft.Database.Migrator.Tests/SqlServer/ExistingSqlDatabaseFixture.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading;
+using Dapper;
+
+namespace SimpleSoft.Database.Migrator.Tests.SqlServer
+{
+    public class ExistingSqlDatabaseFixture : IDisposable
+    {
+        public const string DatabaseName = "MigratorTest";
+
+        public const string ConnectionString =
+                "Data Source=.; Initial Catalog=MigratorTest; Integrated Security=True; Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=True;App=SimpleSoft.Database.Migrator.Tests.SqlServer"
+            ;
+
+        private const string MasterConnectionString =
+                "Data Source=.; Initial Catalog=master; Integrated Security=True; Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=True;App=SimpleSoft.Database.Migrator.Tests.SqlServer"
+            ;
+
+        public ExistingSqlDatabaseFixture()
+        {
+            CreateDatabaseIfNeeded(DatabaseName);
+
+            Connection = new SqlConnection(ConnectionString);
+            Context = new MigratorTestContext(Connection);
+        }
+
+        public DbConnection Connection { get; private set; }
+
+        public MigratorTestContext Context { get; private set; }
+
+        #region Implementation of IDisposable
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Context?.Dispose();
+            Connection?.Dispose();
+
+            Context = null;
+            Connection = null;
+        }
+
+        #endregion
+
+        private static void CreateDatabaseIfNeeded(string databaseName)
+        {
+            using (var connection = new SqlConnection(MasterConnectionString))
+            {
+                connection.Open();
+
+                var timeout = connection.ConnectionTimeout;
+
+                var dbId = connection.QuerySingleOrDefault<long?>(
+                    "SELECT DB_ID(@databaseName) as DatabaseId", new
+                    {
+                        databaseName
+                    }, null, timeout);
+                if (dbId.HasValue)
+                    return;
+
+                connection.Execute("CREATE DATABASE " + databaseName, null, null, timeout);
+                connection.Close();
+
+                using (var context = new MigratorTestContext(connection))
+                {
+                    var manager = new SqlServerMigrationManager<MigratorTestContext>(
+                        context, LoggingManager.CreateTestLogger<SqlServerMigrationManager<MigratorTestContext>>());
+
+                    manager.PrepareDatabaseAsync(CancellationToken.None)
+                        .ConfigureAwait(false)
+                        .GetAwaiter()
+                        .GetResult();
+                }
+            }
+        }
+    }
+}
--- test/SimpleSoft.Database.Migrator.Tests/SqlServer/SqlServerMigrationManagerTests.cs
+++ test/SimpleSoft.Database.Migrator.Tests/SqlServer/SqlServerMigrationManagerTests.cs
@@ -1,11 +1,32 @@
-﻿namespace SimpleSoft.Database.Migrator.Tests.SqlServer
-{
-    public class SqlServerMigrationManagerTests
-    {
-        //[Fact]
-        //public async Task GivenANewMigrationWhenAppliedThenAnHistoryEntryMustBeAdded()
-        //{
+﻿using System;
+using Xunit;
 
-        //}
+namespace SimpleSoft.Database.Migrator.Tests.SqlServer
+{
+    public class SqlServerMigrationManagerTests : IClassFixture<ExistingSqlDatabaseFixture>
+    {
+        private readonly ExistingSqlDatabaseFixture _fixture;
+
+        public SqlServerMigrationManagerTests(ExistingSqlDatabaseFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void GivenANewManagerWhenPassingNullParametersThenArgumentNullExceptionMustBeThrown()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var manager = new SqlServerMigrationManager<MigratorTestContext>(
+                    null, LoggingManager.CreateTestLogger<SqlServerMigrationManager<MigratorTestContext>>());
+                Assert.NotNull(manager);
+            });
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var manager = new SqlServerMigrationManager<MigratorTestContext>(_fixture.Context, null);
+                Assert.NotNull(manager);
+            });
+        }
     }
 }
