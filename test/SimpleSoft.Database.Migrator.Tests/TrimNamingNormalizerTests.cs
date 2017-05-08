83ee429cfc045fa7af2f52e4d9c8e50c9d6746c6
João Simões <nogravity00@outlook.com>
2017-05-08 13:59:50 +0100

    Added tests for trim naming provider

---
+++ test/SimpleSoft.Database.Migrator.Tests/TrimNamingNormalizerTests.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+
+namespace SimpleSoft.Database.Migrator.Tests
+{
+    public class TrimNamingNormalizerTests
+    {
+        [Fact]
+        public void GivenANormalizerWhenPassingNullNameThenNullMustBeReturned()
+        {
+            var normalizer = new TrimNamingNormalizer();
+
+            var normalizedName = normalizer.Normalize(null);
+
+            Assert.Null(normalizedName);
+        }
+
+        [Fact]
+        public void GivenANormalizerWhenPassingWhitespaceNameThenEmptyMustBeReturned()
+        {
+            var normalizer = new TrimNamingNormalizer();
+
+            var normalizedName = normalizer.Normalize("      ");
+
+            Assert.NotNull(normalizedName);
+            Assert.Same(string.Empty, normalizedName);
+        }
+
+        [Fact]
+        public void GivenAgNormalizerWhenPassingNotTrimNameThenTrimMustBeReturned()
+        {
+            const string nonNormalizedName = "   !Hello.123.World!   ";
+
+            var normalizer = new TrimNamingNormalizer();
+
+            var normalizedName = normalizer.Normalize(nonNormalizedName);
+
+            Assert.NotNull(normalizedName);
+            Assert.Equal("!Hello.123.World!", normalizedName);
+        }
+
+        [Fact]
+        public void GivenANormalizerWhenPassingNormalizedNameThenEqualValueMustBeReturned()
+        {
+            const string nonNormalizedName = "!Hello.123.World!";
+
+            var normalizer = new TrimNamingNormalizer();
+
+            var normalizedName = normalizer.Normalize(nonNormalizedName);
+
+            Assert.NotNull(normalizedName);
+            Assert.Equal(nonNormalizedName, normalizedName);
+        }
+    }
+}
