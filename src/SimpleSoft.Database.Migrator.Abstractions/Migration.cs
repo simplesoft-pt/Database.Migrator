eae7b57125a19760cf130b0f2b2bf884b31e3363
João Simões <nogravity00@outlook.com>
2017-05-19 14:41:11 +0100

    NotImplementedException is now thrown
    This tries to prevent unexpected behavior if a developer creates an Apply script but not the Rollback, without beeing explicit

--- src/SimpleSoft.Database.Migrator.Abstractions/Migration.cs
+++ src/SimpleSoft.Database.Migrator.Abstractions/Migration.cs
@@ -56,13 +56,15 @@ namespace SimpleSoft.Database.Migrator
         /// <inheritdoc />
         public virtual Task ApplyAsync(CancellationToken ct)
         {
-            return Task.FromResult(true);
+            throw new NotImplementedException(
+                "This migration apply method was not overriden. This is intended to prevent the migration history to change without the developer beeing explicit by overriden this method.");
         }
 
         /// <inheritdoc />
         public virtual Task RollbackAsync(CancellationToken ct)
         {
-            return Task.FromResult(true);
+            throw new NotImplementedException(
+                "This migration rollback method was not overriden. This is intended to prevent the migration history to change without the developer beeing explicit by overriden this method.");
         }
     }
 }
