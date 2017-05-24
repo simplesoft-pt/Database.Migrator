1240eab502ca93cb0f37a3de5d699d4b15def1ea
João Simões <nogravity00@outlook.com>
2017-05-24 15:53:21 +0100

    #6 Implemented management logic

--- src/SimpleSoft.Database.Migrator.Oracle/OracleMigrationManager.cs
+++ src/SimpleSoft.Database.Migrator.Oracle/OracleMigrationManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -60,34 +61,90 @@ WHERE
         }
 
         /// <inheritdoc />
-        protected override Task CreateMigrationsHistoryAsync(CancellationToken ct)
+        protected override async Task CreateMigrationsHistoryAsync(CancellationToken ct)
         {
-            throw new NotImplementedException();
+            await Context.ExecuteAsync(@"
+CREATE TABLE DB_MIGRATOR_HISTORY(
+    CONTEXT_NAME VARCHAR(256) NOT NULL,
+    MIGRATION_ID VARCHAR(128) NOT NULL,
+    CLASS_NAME VARCHAR(512) NOT NULL,
+    APPLIED_ON TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY (CONTEXT_NAME, MIGRATION_ID)
+)")
+                .ConfigureAwait(false);
         }
 
         /// <inheritdoc />
-        protected override Task InsertMigrationEntryAsync(string contextName, string migrationId, string className, DateTimeOffset appliedOn,
-            CancellationToken ct)
+        protected override async Task InsertMigrationEntryAsync(string contextName, string migrationId, string className, DateTimeOffset appliedOn, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            await Context.ExecuteAsync(@"
+INSERT INTO DB_MIGRATOR_HISTORY(CONTEXT_NAME, MIGRATION_ID, CLASS_NAME, APPLIED_ON)
+VALUES (:ContextName, :MigrationId, :ClassName, :AppliedOn)", new
+                {
+                    ContextName = contextName,
+                    MigrationId = migrationId,
+                    ClassName = className,
+                    AppliedOn = appliedOn
+                })
+                .ConfigureAwait(false);
         }
 
         /// <inheritdoc />
-        protected override Task<IReadOnlyCollection<string>> GetAllMigrationsAsync(string contextName, CancellationToken ct)
+        protected override async Task<IReadOnlyCollection<string>> GetAllMigrationsAsync(string contextName, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            var result = await Context.Query<string>(@"
+SELECT 
+  MIGRATION_ID
+FROM DB_MIGRATOR_HISTORY
+WHERE
+  CONTEXT_NAME = :ContextName
+ORDER BY
+  CONTEXT_NAME ASC, MIGRATION_ID DESC", new
+                {
+                    ContextName = contextName
+                })
+                .ConfigureAwait(false);
+
+            return result as IReadOnlyCollection<string> ?? result.ToList();
         }
 
         /// <inheritdoc />
-        protected override Task<string> GetMostRecentMigrationEntryIdAsync(string contextName, CancellationToken ct)
+        protected override async Task<string> GetMostRecentMigrationEntryIdAsync(string contextName, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            var migrationId = await Context.QuerySingleOrDefaultAsync<string>(@"
+SELECT MIGRATION_ID
+FROM (
+  SELECT 
+    MIGRATION_ID
+  FROM DB_MIGRATOR_HISTORY
+  WHERE
+    CONTEXT_NAME = :ContextName
+  ORDER BY
+    CONTEXT_NAME ASC, MIGRATION_ID DESC
+)
+WHERE
+  ROWNUM < 2", new
+                {
+                    ContextName = contextName
+                })
+                .ConfigureAwait(false);
+
+            return migrationId;
         }
 
         /// <inheritdoc />
-        protected override Task DeleteMigrationEntryByIdAsync(string contextName, string migrationId, CancellationToken ct)
+        protected override async Task DeleteMigrationEntryByIdAsync(string contextName, string migrationId, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            await Context.ExecuteAsync(@"
+DELETE FROM DB_MIGRATOR_HISTORY 
+WHERE 
+    CONTEXT_NAME = :ContextName
+    AND MIGRATION_ID = :MigrationId", new
+                {
+                    ContextName = contextName,
+                    MigrationId = migrationId
+                })
+                .ConfigureAwait(false);
         }
 
         #endregion
