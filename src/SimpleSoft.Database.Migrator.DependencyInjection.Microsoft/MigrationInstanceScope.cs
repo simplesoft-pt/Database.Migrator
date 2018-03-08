520d7667ab57243ee451e58f17095a6200321221
João Simões <nogravity00@outlook.com>
2018-03-08 18:19:46 +0000

    #23 facade implementation for Microsoft DI

---
+++ src/SimpleSoft.Database.Migrator.DependencyInjection.Microsoft/MigrationInstanceScope.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleSoft.Database.Migrator
+{
+    /// <summary>
+    /// Scope used to resolve instances using a <see cref="IServiceScope"/>.
+    /// </summary>
+    public class MigrationInstanceScope : IMigrationInstanceScope
+    {
+        private readonly IServiceScope _scope;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="scope">The service scope</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public MigrationInstanceScope(IServiceScope scope)
+        {
+            _scope = scope ?? throw new ArgumentNullException(nameof(scope));
+        }
+
+        /// <inheritdoc />
+        ~MigrationInstanceScope() => Dispose(false);
+
+        /// <inheritdoc />
+        public object GetInstance(Type type) => _scope.ServiceProvider.GetService(type);
+
+        /// <inheritdoc />
+        public T GetInstance<T>() => _scope.ServiceProvider.GetService<T>();
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting
+        /// unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">Is the instance beeing disposed?</param>
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+                _scope.Dispose();
+        }
+    }
+}
---
+++ src/SimpleSoft.Database.Migrator.DependencyInjection.Microsoft/MigrationInstanceScopeFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleSoft.Database.Migrator
+{
+    /// <summary>
+    /// Factory used to build <see cref="IMigrationInstanceScope"/> instances
+    /// using a <see cref="IServiceScopeFactory"/>.
+    /// </summary>
+    public class MigrationInstanceScopeFactory : IMigrationInstanceScopeFactory
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="scopeFactory">The service scope factory to be used</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public MigrationInstanceScopeFactory(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory ?? throw new ArgumentNullException(nameof(scopeFactory));
+        }
+
+        /// <inheritdoc />
+        public IMigrationInstanceScope Build() => new MigrationInstanceScope(_scopeFactory.CreateScope());
+    }
+}
