504d352c8cd8f7adac28be5cb72946dce97ca5f4
João Simões <nogravity00@outlook.com>
2018-03-08 14:09:12 +0000

    #23 Interfaces to abstract DI

---
+++ src/SimpleSoft.Database.Migrator.Abstractions/DependencyInjection/IMigrationInstanceScope.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimpleSoft.Database.Migrator.DependencyInjection
+{
+    /// <summary>
+    /// Scope used to resolve instances
+    /// </summary>
+    public interface IMigrationInstanceScope : IDisposable
+    {
+        /// <summary>
+        /// Gets an instance for the given type.
+        /// </summary>
+        /// <param name="type">The instance type</param>
+        /// <returns>The resolved instance</returns>
+        object GetInstance(Type type);
+
+        /// <summary>
+        /// Gets an instance for the given type.
+        /// </summary>
+        /// <typeparam name="T">The instance type</typeparam>
+        /// <returns>The resolved instance</returns>
+        T GetInstance<T>();
+    }
+}
---
+++ src/SimpleSoft.Database.Migrator.Abstractions/DependencyInjection/IMigrationInstanceScopeFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleSoft.Database.Migrator.DependencyInjection
+{
+    /// <summary>
+    /// Factory used to build <see cref="IMigrationInstanceScope"/> instances.
+    /// </summary>
+    public interface IMigrationInstanceScopeFactory
+    {
+        /// <summary>
+        /// Builds a new instance scope
+        /// </summary>
+        /// <returns>The instance scope</returns>
+        IMigrationInstanceScope Build();
+    }
+}
