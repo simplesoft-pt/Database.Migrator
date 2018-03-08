1f8b533f982f2847cc6fd9a45509fef8aeed536c
João Simões <nogravity00@outlook.com>
2018-03-08 14:51:17 +0000

    Helper methods for resolving instances

---
+++ src/SimpleSoft.Database.Migrator.Abstractions/DependencyInjection/MigrationInstanceScopeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace SimpleSoft.Database.Migrator
+{
+    /// <summary>
+    /// Extensions for <see cref="IMigrationInstanceScope"/> instances.
+    /// </summary>
+    public static class MigrationInstanceScopeExtensions
+    {
+        /// <summary>
+        /// Gets a required instance for the given type.
+        /// </summary>
+        /// <param name="scope">The instance scope</param>
+        /// <param name="type">The instance type</param>
+        /// <returns>The resolved instance</returns>
+        /// <exception cref="RequiredInstanceIsNullException"></exception>
+        public static object GetRequiredInstance(this IMigrationInstanceScope scope, Type type)
+        {
+            var instance = scope.GetInstance(type);
+            if (instance == null)
+                throw new RequiredInstanceIsNullException(type);
+
+            return instance;
+        }
+
+        /// <summary>
+        /// Gets a required instance for the given type.
+        /// </summary>
+        /// <typeparam name="T">The instance type</typeparam>
+        /// <param name="scope">The instance scope</param>
+        /// <returns>The resolved instance</returns>
+        /// <exception cref="RequiredInstanceIsNullException"></exception>
+        public static T GetRequiredInstance<T>(this IMigrationInstanceScope scope) =>
+            (T) scope.GetInstance(typeof(T));
+    }
+}
---
+++ src/SimpleSoft.Database.Migrator.Abstractions/DependencyInjection/RequiredInstanceIsNullException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace SimpleSoft.Database.Migrator
+{
+    /// <summary>
+    /// Thrown when resolving an instance returns null
+    /// </summary>
+    public class RequiredInstanceIsNullException : InvalidOperationException
+    {
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="instanceType">The type beeing resolved</param>
+        public RequiredInstanceIsNullException(Type instanceType)
+            : base($"Failed to get a required instance for type '{instanceType.FullName}'")
+        {
+            InstanceType = instanceType;
+        }
+
+        /// <summary>
+        /// The instance type beeing resolved
+        /// </summary>
+        public Type InstanceType { get; }
+    }
+}
