8bf7a7f166db9da1490421d55efe0381b397f79c
João Simões <nogravity00@outlook.com>
2018-03-12 12:23:26 +0000

    Helper for .NET 4.5.1 tasks

---
+++ src/SimpleSoft.Database.Migrator/Internal/TaskEx.cs
@@ -0,0 +1,14 @@
+﻿#if NET451
+
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace SimpleSoft.Database.Migrator
+{
+    internal static class TaskEx
+    {
+        public static readonly Task CompletedTask = Task.FromResult(true);
+    }
+}
+
+#endif
